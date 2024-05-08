35f9842fc5476da0080e835413f3f2990ae5ecd5
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-05-08 14:35:37 -0600

    Setting up arrays

---
+++ src/ZombieGame.java
@@ -0,0 +1,9 @@
+public class ZombieGame {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to our zombie killer game.");
+
+        String backpack[] = {"shotgun", "assault rifle", "sniper rifle"};
+        String zombies[] = {"walker", "shrieker", "vile walker"};
+    }
+}
